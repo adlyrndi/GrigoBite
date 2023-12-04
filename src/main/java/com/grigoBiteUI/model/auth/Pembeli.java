891feda595cafe963c1bc35aefe90346fcff50c1
Ikra Bhaktiananda <ikra.bhaktiananda@ui.ac.id>
2023-12-04 22:30:12 +0700

    add pesanan controller

--- src/main/java/com/grigoBiteUI/model/auth/Pembeli.java
+++ src/main/java/com/grigoBiteUI/model/auth/Pembeli.java
@@ -1,17 +1,19 @@
 package com.grigoBiteUI.model.auth;
 
 import com.grigoBiteUI.model.Pesanan;
-import com.grigoBiteUI.model.auth.User;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
-import jakarta.persistence.*;
 import java.util.List;
 
 @Data
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
--- src/main/java/com/grigoBiteUI/repository/PesananRepository.java
+++ src/main/java/com/grigoBiteUI/repository/PesananRepository.java
@@ -2,17 +2,21 @@ package com.grigoBiteUI.repository;
 
 import com.grigoBiteUI.model.Pesanan;
 import lombok.NonNull;
-import org.checkerframework.checker.index.qual.NonNegative;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface PesananRepository extends JpaRepository<Pesanan, Long> {
 
     @NonNull
     List<Pesanan> findAll();
-    Pesanan findById(long id);
+
+
+    Pesanan getById(long id);
 
     List<Pesanan> findByPembeliId(long pembeliId);
 
     List<Pesanan> findByPenjualId(long penjualId);
+
+    void deleteById(long id);
 }
